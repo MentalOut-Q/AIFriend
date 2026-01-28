7fb2fab04e9658e1161b7ee1adc999bab262faf6
siyuan qian <1958044381@qq.com>
2026-01-28 11:43:05 +0800

    实现数据库

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from web.models.user import UserProfile
 
 # Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)  #逗号千万不要删！！！！ 如果不加这个user, 就是以下拉菜单的方式, 加了就是单独开一个页面加载
+
