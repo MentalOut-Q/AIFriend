a60f7e9aafc55f0e713c2e3a05dc4a8da07b4ba3
siyuan qian <1958044381@qq.com>
2026-01-28 11:13:33 +0800

    实现登录, 注册功能

--- frontend/src/views/user/account/RegisterIndex.vue
+++ frontend/src/views/user/account/RegisterIndex.vue
@@ -3,7 +3,25 @@
 </script>
 
 <template>
-注册
+  <div class="flex justify-center mt-10">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <legend class="fieldset-legend">Register</legend>
+
+      <label class="label">用户名</label>
+      <input type="email" class="input" placeholder="Email" />
+
+      <label class="label">密码</label>
+      <input type="password" class="input" placeholder="Password" />
+
+      <label class="label">确认密码</label>
+      <input type="password" class="input" placeholder="Password" />
+
+      <button class="btn btn-neutral mt-4">注册</button>
+      <div class="flex justify-end">
+        <RouterLink :to="{name: 'user-account-login-index'}" class="btn btn-sm btn-ghost text-gray-500">登录</RouterLink>
+      </div>
+    </fieldset>
+  </div>
 </template>
 
 <style scoped>
