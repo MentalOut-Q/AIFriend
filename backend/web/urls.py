6484b598ef92b598941ac6ac96201a94a80dce08
siyuan qian <1958044381@qq.com>
2026-02-12 22:15:21 +0800

    实现首页前后端

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -9,6 +9,7 @@ from web.views.create.character.get_list import GetListCharacterView
 from web.views.create.character.get_single import GetSingleCharacterView
 from web.views.create.character.remove import RemoveCharacterView
 from web.views.create.character.update import UpdateCharacterView
+from web.views.homepage.index import HomepageIndexView
 from web.views.index import index
 from web.views.user.account.login import LoginView
 from web.views.user.account.logout import LogoutView
@@ -31,6 +32,7 @@ urlpatterns = [
     path('api/create/character/remove/', RemoveCharacterView.as_view()),
     path('api/create/character/get_single/', GetSingleCharacterView.as_view()),
     path('api/create/character/get_list/', GetListCharacterView.as_view()),
+    path('api/homepage/index/', HomepageIndexView.as_view()),
 
 
     path('', index), #如果url是''里的网址(路径), 就会自动调用index这个函数(写在views里的)
