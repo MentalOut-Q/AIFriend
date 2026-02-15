5d370966da3838213e26910b7130ac84bdab969a
siyuan qian <1958044381@qq.com>
2026-02-15 16:50:14 +0800

    实现好友页面后端

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from web.models.friend import Friend
 from web.models.user import UserProfile
 from web.models.character import Character
 
@@ -6,8 +8,12 @@ from web.models.character import Character
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    raw_id_fields = ('user',)  #逗号千万不要删！！！！ 如果不加这个user, 就是以下拉菜单的方式, 加了就是单独开一个页面加载
+    raw_id_fields = ('user',)  #逗号千万不要删！！！！ 如果不加这个user(这是个外键, 孩子), 就是以下拉菜单的方式, 加了就是单独开一个页面加载
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    raw_id_fields = ('me', 'character',) # 把所有的外键全部加进来
